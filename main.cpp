26e7d4b27358355743084382ff79e57802539153
Ethan Lowenthal <ethanmlowenthal@gmail.com>
2023-07-25 02:33:45 -0700

    ImGui + SDL working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-
+#include "Window.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Window window {};
+
+    bool done = false;
+
+    while (!done) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT)
+                done = true;
+            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window.window))
+                done = true;
+        }
+
+        window.new_frame();
+
+        {
+            float f;
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+            ImGui::End();
+        }
+
+        window.render();
+    }
     return 0;
 }
